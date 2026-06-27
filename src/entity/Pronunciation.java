6179b80ca2977a85fa1d88e303f9f43a07a92ac9
Vqhuy1212 <vqhuy1212@gmail.com>
2026-06-27 22:15:55 +0700

    Pronunciation

--- src/entity/Pronunciation.java
+++ src/entity/Pronunciation.java
@@ -1,4 +1,37 @@
 package entity;
 
 public class Pronunciation {
+
+    // Phiên âm IPA của từ
+    private String ipa;
+
+    /**
+     * Constructor
+     * @param ipa Phiên âm IPA
+     */
+    public Pronunciation(String ipa) {
+        this.ipa = ipa;
+    }
+
+    /**
+     * Lấy phiên âm
+     */
+    public String getIpa() {
+        return ipa;
+    }
+
+    /**
+     * Cập nhật phiên âm
+     */
+    public void setIpa(String ipa) {
+        this.ipa = ipa;
+    }
+
+    /**
+     * Hiển thị thông tin đối tượng
+     */
+    @Override
+    public String toString() {
+        return ipa;
+    }
 }
